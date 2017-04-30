94048f63ab87ff9f65029d702bbfebcfbf4ea0f5
Liam Whan <liam.whan@gmail.com>
2017-04-30 12:29:41 +1000

    Setup Unit Tests (better late than never)

---
+++ __tests__/FFProcess.test.ts
@@ -0,0 +1,11 @@
+import {expect} from 'chai';
+import {IProcessOptions} from "../src/FFInterfaces";
+
+
+describe("FFProbe should return ", () => {
+    let opts:IProcessOptions = {
+        input: "./vid/large.mkv",
+        process: "ffprobe"
+    };
+
+});
