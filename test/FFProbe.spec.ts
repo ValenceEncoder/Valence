92210deb8005788a2c4207cdd416632808a2806c
Liam Whan <liam.whan@gmail.com>
2017-05-02 22:21:27 +1000

    Add Creative Commons video to repo for travis tests

--- test/FFProbe.spec.ts
+++ test/FFProbe.spec.ts
@@ -2,10 +2,10 @@ import * as chai from 'chai';
 import {FFProbe, FFMpeg} from '../src/FFProcess';
 import * as mocha from 'mocha';
 
-const expect          = chai.expect;
+const expect = chai.expect;
 
 describe("FFProbe", function () {
-    let ffprobeInstance = new FFProbe({input: "vid/large.mkv"});
+    let ffprobeInstance = new FFProbe({input: "vid/CCTest.mkv"});
     describe("#constructor()", function () {
         it("be an instance of the FFProbe class", function () {
             expect(ffprobeInstance).to.be.instanceof(FFProbe);
@@ -15,11 +15,11 @@ describe("FFProbe", function () {
     describe("#run()", function () {
         it("should return a JSON object of the correct form", function (done) {
             let expected = {
-                videoInfo: {codec_name: 'h264', duration: 3600.179, size: 931237935},
+                videoInfo: {codec_name: 'h264', duration: 20.256, size: 11808593},
                 audioInfo: {codec_name: 'ac3'}
             };
 
-            ffprobeInstance.run().on(FFProbe.EVENT_OUTPUT, (result:any) => {
+            ffprobeInstance.run().on(FFProbe.EVENT_OUTPUT, (result: any) => {
                 expect(result).to.deep.equal(expected);
                 done();
             });
