23bcf90e5810840a442340817a324e071ad03ef5
Liam Whan <liam.whan@gmail.com>
2017-05-03 14:38:02 +1000

    Change outdir

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ export FFMPEG_ARCHIVE="ffmpeg-git-64bit-static.tar.xz"
 export FFMPEG_ARCHIVE_DIR="./bin/ffmpeg/ffmpeg-git-20170417-64bit-static"
 export FFMPEG=$FFMPEG_ARCHIVE_DIR/ffmpeg
 export FFPROBE=$FFMPEG_ARCHIVE_DIR/ffprobe
-export FFMPEG_OUTDIR="./bin/ffmpeg"
+export FFMPEG_OUTDIR="./ffmpeg/bin"
 mkdir -p $FFMPEG_OUTDIR
 wget https://johnvansickle.com/ffmpeg/builds/$FFMPEG_ARCHIVE && tar -xvf $FFMPEG_ARCHIVE -C $FFMPEG_OUTDIR
 
