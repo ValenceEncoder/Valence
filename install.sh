f3577cffd6d268810c97158cf8b5c7283e48eaa6
Liam Whan <liam.whan@gmail.com>
2017-05-05 17:07:36 +1000

    Update build script to validate checksum

--- install.sh
+++ install.sh
@@ -1,13 +1,27 @@
 #!/usr/bin/env bash
 
-export FFMPEG_ARCHIVE="ffmpeg-git-64bit-static.tar.xz"
-export FFMPEG_ARCHIVE_DIR="./ffmpeg/bin/ffmpeg-git-20170417-64bit-static"
-export FFMPEG=$FFMPEG_ARCHIVE_DIR/ffmpeg
-export FFPROBE=$FFMPEG_ARCHIVE_DIR/ffprobe
-export FFMPEG_OUTDIR="./ffmpeg/bin"
-mkdir -p $FFMPEG_OUTDIR
-wget https://johnvansickle.com/ffmpeg/builds/$FFMPEG_ARCHIVE && tar -xvf $FFMPEG_ARCHIVE -C $FFMPEG_OUTDIR
+FFMPEG_OUTDIR="./ffmpeg/bin"
+FFMPEG_ARCHIVE="ffmpeg-git-64bit-static.tar.xz"
+FFMPEG_CHECKSUM="ffmpeg-git-64bit-static.tar.xz.md5"
+FFMPEG_ARCHIVE_DIR="${FFMPEG_OUTDIR}/ffmpeg-git-20170417-64bit-static"
+FFMPEG=${FFMPEG_ARCHIVE_DIR}/ffmpeg
+FFPROBE=${FFMPEG_ARCHIVE_DIR}/ffprobe
+mkdir -p ${FFMPEG_OUTDIR}
+wget https://johnvansickle.com/ffmpeg/builds/${FFMPEG_ARCHIVE} && tar -xvf ${FFMPEG_ARCHIVE} -C ${FFMPEG_OUTDIR}
+wget https://johnvansickle.com/ffmpeg/builds/${FFMPEG_CHECKSUM}
+# Compare Checksums
+GENSUM="$(md5sum ${FFMPEG_ARCHIVE})"
+ACTSUM="$(cat ${FFMPEG_CHECKSUM})"
 
-mv $FFMPEG $FFMPEG_OUTDIR
-mv $FFPROBE $FFMPEG_OUTDIR
-rm -rf $FFMPEG_ARCHIVE_DIR
+if [ "${GENSUM}" == "${ACTSUM}" ]; then
+    echo "Checksums Validated"
+else
+     echo "Checksum Failed"
+     exit 1
+fi
+
+mv ${FFMPEG} ${FFMPEG_OUTDIR}
+mv ${FFPROBE} ${FFMPEG_OUTDIR}
+rm -rf ${FFMPEG_ARCHIVE_DIR}
+rm ${FFMPEG_ARCHIVE}
+rm ${FFMPEG_CHECKSUM}
