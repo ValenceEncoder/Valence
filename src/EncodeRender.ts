a432445cea44f62ad979d5af00217da0322e4e09
Liam Whan <liam.whan@gmail.com>
2017-05-06 18:29:28 +1000

    Add FFprobe spawn in Renderer

--- src/EncodeRender.ts
+++ src/EncodeRender.ts
@@ -1,9 +1,38 @@
 import {remote, ipcRenderer} from 'electron';
 import {IPCEventType} from './ElectronUtils';
-const BrowserWindow = remote.BrowserWindow;
+import * as Path from "path";
+import {FFMpeg, FFProbe} from "./FFProcess";
+import {IFFProbeOutput, IProcessOptions} from "./FFInterfaces";
+import BrowserWindow = Electron.BrowserWindow;
 
-ipcRenderer.on(IPCEventType.SPAWN_ENCODER, function(event, message, fromWindowId) {
-    document.getElementById('message').innerText = message;
-    const fromWindow = BrowserWindow.fromId(fromWindowId);
-    fromWindow.webContents.send(IPCEventType.ENCODE_COMPLETED)
+const browserWindow = remote.BrowserWindow;
+let ffprobe:FFProbe;
+let ffmpeg:FFMpeg;
+let parentWindow: BrowserWindow;
+
+function onProbeComplete(result:IFFProbeOutput):void {
+   console.log(result);
+
+}
+
+
+ipcRenderer.on(IPCEventType.SPAWN_ENCODER, function(event, inputFile:string, parentId:number) {
+    console.log(IPCEventType.SPAWN_ENCODER);
+    parentWindow = browserWindow.fromId(parentId);
+    document.getElementById('heading').innerText = `Encoding ${Path.basename(inputFile)}`;
+
+    let ffprobeOptions:IProcessOptions = {
+        input: inputFile,
+        process: 'ffprobe'
+    };
+    ffprobe = new FFProbe(ffprobeOptions);
+    ffprobe.on(FFProbe.EVENT_OUTPUT, onProbeComplete);
+    ffprobe.on(FFProbe.EVENT_ERROR, (err:Error) => {
+        console.error(err);
+    });
+    ffprobe.run();
+
+
+    // const fromWindow = BrowserWindow.fromId(fromWindowId);
+    // fromWindow.webContents.send(IPCEventType.ENCODE_COMPLETED)
 });
