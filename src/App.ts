2f92abbb67a8d7075a9234627ef9675bb72c3724
Liam Whan <liam.whan@gmail.com>
2017-05-14 17:00:34 +1000

    Remove redudant import

--- src/App.ts
+++ src/App.ts
@@ -1,10 +1,8 @@
 import {app, BrowserWindow} from 'electron';
 import Main from './Main';
 import * as Path from 'path';
-// import {enableLiveReload} from 'electron-compile';
-// enableLiveReload();
-import {IConfig} from "./lib/FFInterfaces";
 const appPath = require('electron').app.getAppPath();
+
 global.ValenceConfig = require(Path.join(appPath,  '/src/vendor/electron-node-config'));
 global.ValenceConfig.bin.ffprobe = Path.join(appPath, global.ValenceConfig.bin.ffprobe);
 global.ValenceConfig.bin.ffmpeg = Path.join(appPath, global.ValenceConfig.bin.ffmpeg);
